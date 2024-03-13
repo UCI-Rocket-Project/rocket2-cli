f2aaae4270beee1c5a2470705d8a03f6c149ee54
Shah <aashay.as99@gmail.com>
2024-03-13 15:57:19 -0700

    Update

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ main_thread = None
 main_thread_end_evt = threading.Event()
 main_state = None 
 
+# OUr software is quite simmilar to mavporxy. We can study its code
+# for proper error handling techniques for various compoenets that
+# we use so that we can have higher quality code.
+# We stole the idea about having 2 threads and their roles from there.
 
 def shlex_quotes(value):
     '''see http://stackoverflow.com/questions/6868382/python-shlex-split-ignore-single-quotes'''
@@ -50,6 +54,63 @@ def cmd_log(args):
 def cmd_monitor(args):
     pass
 
+# Refer:https://github.com/UCI-Rocket-Project/rocket2-overview?tab=readme-ov-file#gse-command-packet
+GSE_COMMAND_TUPLE_FORMAT = ['igniterFire'
+    ,'solenoidStateGn2Fill'
+    ,'solenoidStateGn2Vent'
+    ,'solenoidStateMvasFill'
+    ,'solenoidStateMvasVent'
+    ,'solenoidStateMvas'
+    ,'solenoidStateLoxFill'
+    ,'solenoidStateLoxVent'
+    ,'solenoidStateLngFill'
+    ,'solenoidStateLngVent'
+]
+
+#Refer: https://github.com/UCI-Rocket-Project/rocket2-overview?tab=readme-ov-file#ecu-command-packet
+ECU_COMMAND_TUPLE_FORMAT = [
+    'solenoidStateGn2Vent',
+    'solenoidStatePv1',
+    'solenoidStatePv2',
+    'solenoidStateVent'
+]
+def cmd_command(args):
+    if args[0] == 'gse':
+        if args[1] == 'setall':
+            values = [int(x) for x in args[2].split(',')]
+            if len(values) != 10:
+                print("Too many or too few arguments in:", values)
+            data = struct.pack('??????????', *values)
+            for f in main_state.write_fds:
+                for c in main_state.conns:
+                    if c.fileno() == f:
+                        c.send(data)
+        elif args[1] == 'set':
+            # Need to track/query state to implement this otherwise all others will
+            # be reset
+            k,v = args[2].split('=')
+            try:
+                idx = GSE_COMMAND_TUPLE_FORMAT.index(k)
+            except ValueError:
+                print(f"Could not find field {k}")
+            values = 
+
+
+    elif args[0] == 'ecu':
+        if args[1] == 'setall':
+            values = [int(x) for x in args[2].split(',')]
+            if len(values) != 4:
+                print("Too many or too few arguments in:", values)
+            data = struct.pack('????', *values)
+            for f in main_state.write_fds:
+                for c in main_state.conns:
+                    if c.fileno() == f:
+                        c.send(data)
+
+
+
+
+
 class MainState:
     def __init__(self):
         self.conns = []
@@ -63,6 +124,7 @@ class MainState:
             "output" : (cmd_output, "Manage output connections for commands"),
             "log" : (cmd_log, "Log specific selected messages to a file"),
             "monitor" : (cmd_monitor, "Monitor specific selected messages by outputting to stdout")
+            "command" : (cmd_command, "Send command")
         }
 
 
@@ -96,6 +158,8 @@ def close_process(exctype, value, traceback):
     sys.__excepthook__(exctype, value, traceback)
 
 def process_telem(conn):
+    # Helpful reference code for connection handling
+    # https://github.com/ArduPilot/pymavlink/blob/f7a2f29607f7f2a72499a3fc19e555aaace51ba8/mavutil.py#L1217
     d = conn.recv(1024)
     if len(d) == 0:
         print("EOF on TCP socket")
@@ -120,6 +184,8 @@ def main_thread():
         r_ready = []
         w_ready = []
         try:
+            # How to use select?
+            # https://github.com/ArduPilot/MAVProxy/blob/e2b03aab02775cad6214990c213ad023800f2112/MAVProxy/mavproxy.py#L1127
             r_ready, w_ready, _ = select.select(
                 main_state.read_fds, 
                 main_state.write_fds, 
